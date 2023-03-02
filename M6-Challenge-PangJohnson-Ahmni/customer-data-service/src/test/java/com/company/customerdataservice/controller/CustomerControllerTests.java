db48cf4dd5109c1b4b18342fae913c59cbc64e9c
Ahmni <ahmni@ku.edu>
2023-03-01 19:24:14 -0600

    final commit

--- M6-Challenge-PangJohnson-Ahmni/customer-data-service/src/test/java/com/company/customerdataservice/controller/CustomerControllerTests.java
+++ M6-Challenge-PangJohnson-Ahmni/customer-data-service/src/test/java/com/company/customerdataservice/controller/CustomerControllerTests.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,19 +16,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+//@WebMvcTest(CustomerController.class)
 @RunWith(SpringRunner.class)
-@WebMvcTest(CustomerController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class CustomerControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private CustomerRepository customerRepo;
-
     @Before
     public void setUp() throws Exception {
         customerRepo.deleteAll();
@@ -48,8 +53,6 @@ public class CustomerControllerTests {
         customer.setCountry("United States");
 
         String inputJson = mapper.writeValueAsString(customer);
-        String outputJson = mapper.writeValueAsString(customer);
-        customerRepo.save(customer);
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/customers")
                                 .content(inputJson)
@@ -62,15 +65,15 @@ public class CustomerControllerTests {
     @Test
     public void shouldDeleteByCustomerAndReturn204StatusCode() throws Exception {
         Customer customer = new Customer();
-        customer.setId(1);
         String inputJson = mapper.writeValueAsString(customer);
-        mockMvc.perform(
-                post("/customers")
-                        .content(inputJson)
-                        .contentType(MediaType.APPLICATION_JSON));
+        //Act...
+        customer = customerRepo.save(customer);
+
+        //Assert...
+        Optional<Customer> customer1 = customerRepo.findById(customer.getId());
 
         mockMvc.perform(MockMvcRequestBuilders
-                        .delete("/customers/1"))
+                        .delete("/customers/{id}", customer.getId()))
                 .andDo(print())
                 .andExpect(status().isNoContent());
     }
@@ -78,14 +81,8 @@ public class CustomerControllerTests {
     @Test
     public void shouldUpdateCustomer() throws Exception {
         Customer customer = new Customer();
-        customer.setId(1);
         String inputJson = mapper.writeValueAsString(customer);
-        mockMvc.perform(
-                post("/customers")
-                        .content(inputJson)
-                        .contentType(MediaType.APPLICATION_JSON));
-        customer.setFirstName("Updated");
-        inputJson = mapper.writeValueAsString(customer);
+
         mockMvc.perform(MockMvcRequestBuilders
                         .put("/customers/")
                         .content(inputJson)
@@ -107,27 +104,43 @@ public class CustomerControllerTests {
         customer.setState("KS");
         customer.setPostalCode(60);
         customer.setCountry("United States");
+
         String inputJson = mapper.writeValueAsString(customer);
-        mockMvc.perform(
-                post("/customers")
-                        .content(inputJson)
-                        .contentType(MediaType.APPLICATION_JSON));
+
+        Customer customer2 = new Customer();
+        customer2.setFirstName("Bob");
+        customer2.setLastName("Marley");
+        customer2.setPhone("222-333-4567");
+        customer2.setCompany("Independent");
+        customer2.setAddress1("456 Timberly Lane");
+        customer2.setAddress2("123 Fake St");
+        customer2.setCity("Lawrence");
+        customer2.setState("KS");
+        customer2.setPostalCode(63);
+        customer2.setCountry("United States");
+        customer2 = customerRepo.save(customer2);
+        //Act...
+        customer = customerRepo.save(customer);
+
         mockMvc.perform(get("/customers/state/KS"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
     }
 
     @Test
     public void shouldReturnCustomerByID() throws Exception {
         Customer customer = new Customer();
-        customer.setId(1);
+
+        //Act...
+        customer = customerRepo.save(customer);
+
+        //Assert...
+        Optional<Customer> customer1 = customerRepo.findById(customer.getId());
         String inputJson = mapper.writeValueAsString(customer);
-        mockMvc.perform(
-                post("/customers")
-                        .content(inputJson)
-                        .contentType(MediaType.APPLICATION_JSON));
-        mockMvc.perform(get("/customers/1"))
+        mockMvc.perform(get("/customers/{id}", customer.getId()))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().json(inputJson));
     }
 }
